1b17e3184032366a72deb85fd30daf49f782f459
ron61 <ron233f@gmail.com>
2020-10-19 19:42:41 +0900

    タイトルのフォントをいじった

--- pages/blog/posts/world-history-of-chocolate.vue
+++ pages/blog/posts/world-history-of-chocolate.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="blog-post">
     <h1>Chocolate is beautiful!</h1>
+    <p class="date">2020/10/19</p>
     <h2>Introduction</h2>
         <p>Chocolate is a preparation of roasted and ground cacao seeds that is made in the form of a liquid, paste, or in a block, which may also be used as a flavoring ingredient in other foods. </p>
         <p>The earliest signs of use are associated with Olmec sites (within what would become Mexico’s post-colonial territory) suggesting consumption of chocolate beverages, dating from 19 centuries BCE.[1][2] The majority of Mesoamerican people made chocolate beverages, including the Maya and Aztecs.[3] The word chocolate is derived from the Spanish word chocolate, deriving in turn from the Classical Nahuatl word xocolātl.[4]</p>
