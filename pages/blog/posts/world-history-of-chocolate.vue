e41541fe26f26aaf63365f92d23ce61df2c92e3e
ron61 <ron233f@gmail.com>
2020-10-19 19:27:06 +0900

    何となくブログのテンプレートをいい感じにした

--- pages/blog/posts/world-history-of-chocolate.vue
+++ pages/blog/posts/world-history-of-chocolate.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    <Top />
-    <nuxt-link to="/">Home</nuxt-link>
-    <p>chocolate is beautiful!</p>
+  <div class="blog-post">
+    <h1>Chocolate is beautiful!</h1>
+    <h2>Introduction</h2>
+        <p>Chocolate is a preparation of roasted and ground cacao seeds that is made in the form of a liquid, paste, or in a block, which may also be used as a flavoring ingredient in other foods. </p>
+        <p>The earliest signs of use are associated with Olmec sites (within what would become Mexico’s post-colonial territory) suggesting consumption of chocolate beverages, dating from 19 centuries BCE.[1][2] The majority of Mesoamerican people made chocolate beverages, including the Maya and Aztecs.[3] The word chocolate is derived from the Spanish word chocolate, deriving in turn from the Classical Nahuatl word xocolātl.[4]</p>
+        <h3>History</h3>
+            <p>The seeds of the cacao tree have an intense bitter taste and must be fermented to develop the flavor. After fermentation, the beans are dried, cleaned, and roasted. The shell is removed to produce cacao nibs, which are then ground to cocoa mass, unadulterated chocolate in rough form. </p>
+        <h3>Meaning</h3>
+            <p>Once the cocoa mass is liquefied by heating, it is called chocolate liquor. The liquor may also be cooled and processed into its two components: cocoa solids and cocoa butter. Baking chocolate, also called bitter chocolate, contains cocoa solids and cocoa butter in varying proportions, without any added sugar. Powdered baking cocoa, which contains more fiber than cocoa butter, can be processed with alkali to produce dutch cocoa. Much of the chocolate consumed today is in the form of sweet chocolate, a combination of cocoa solids, cocoa butter or added vegetable oils, and sugar. Milk chocolate is sweet chocolate that additionally contains milk powder or condensed milk. White chocolate contains cocoa butter, sugar, and milk, but no cocoa solids.</p>
+    <h2>Tasty Chocolate</h2>
+        <p>Chocolate is one of the most popular food types and flavors in the world, and many foodstuffs involving chocolate exist, particularly desserts, including cakes, pudding, mousse, chocolate brownies, and chocolate chip cookies. Many candies are filled with or coated with sweetened chocolate. Chocolate bars, either made of solid chocolate or other ingredients coated in chocolate, are eaten as snacks. Gifts of chocolate molded into different shapes (such as eggs, hearts, coins) are traditional on certain Western holidays, including Christmas, Easter, Valentine's Day, and Hanukkah. Chocolate is also used in cold and hot beverages, such as chocolate milk and hot chocolate, and in some alcoholic drinks, such as creme de cacao.</p>
+        <p>Although cocoa originated in the Americas, West African countries, particularly Côte d'Ivoire and Ghana, are the leading producers of cocoa in the 21st century, accounting for some 60% of the world cocoa supply.</p>
+        <p>With some two million children involved in the farming of cocoa in West Africa, child slavery and trafficking were major concerns in 2018.[5][6] International attempts to improve conditions for children were failing because of persistent poverty, absence of schools, increasing world cocoa demand, more intensive farming of cocoa, and continued exploitation of child labor.[5]</p>
   </div>
 </template>
 
