6be2759c1604e506a5cf5f85de3eebb4c30c8f07
ron61 <ron233f@gmail.com>
2020-10-19 18:04:30 +0900

    ブログの階層を作った

---
+++ pages/blog/posts/world-history-of-chocolate.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Top />
+    <nuxt-link to="/">Home</nuxt-link>
+    <p>chocolate is beautiful!</p>
+  </div>
+</template>
+
+<script>
+export default {
+    layout: "blog"
+}
+</script>
+
+<style>
+
+</style>
